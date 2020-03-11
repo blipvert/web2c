bb7c3f12ac57a8bcb56ed095bf52c1d14bfdc0db
Craig Lennox <blipvert@blipvertica.com>
2020-03-11 13:24:53 -0400

    Added bootstrap.sh and sparse-checkout.list

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -e source/.git ]; then
+    git config --replace-all submodule.source.update '!true'
+    git submodule update --init --progress -- source
+fi
+
+if [ "$(git config --get submodule.source.update)" == '!true' ]; then
+    git config --unset-all submodule.source.update
+fi
+
+git_dir="$( cd source && git rev-parse --git-dir )"
+if [ ! -d "$git_dir" ]; then
+    echo "Bad source/.git directory" >&2
+    exit 1
+fi
+
+if [ ! -f "$git_dir/info/sparse-checkout" ] then
+    cp sparse-checkout.list "$git_dir/info/sparse-checkout"
+    ( cd source && git sparse-checkout init )
+fi
+
+( cd source && ./reautoconf )
